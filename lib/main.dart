e9ae14973b151ae32d0bb655958d54041ddd7af1
Timilehin Arigbede <harriemannie@gmail.com>
2023-01-14 22:15:18 +0100

    onGenerateRoute & onUnknownRoute

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,20 @@ class MyApp extends StatelessWidget {
       home: CategoriesScreen(),
       routes: {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
-        MealDetailScreen.routeName: (ctx) => MealDetailScreen()
+        // MealDetailScreen.routeName: (ctx) => MealDetailScreen()
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        // if (settings.name == '/meal-detail') {
+        //   return ...;
+        // }
+        // else if (settings == '/something-else') {
+        //   return ...;
+        // }
+        // return MaterialPageRoute(builder: (ctx) => CategoriesScreen())
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen())
       },
     );
   }
