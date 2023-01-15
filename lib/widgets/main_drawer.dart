46e28b04c5869d20e14d13fa250eff3884bd1f93
Timilehin Arigbede <harriemannie@gmail.com>
2023-01-15 23:49:14 +0100

    Adding a Custom Drawer

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({Key key}) : super(key: key);
+
+  Widget buildListTile(String title, IconData icon) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 24,
+            fontWeight: FontWeight.bold),
+      ),
+      onTap: () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            // You can set color through box decoration but since you are not setting other properties like border, color property is sufficient like below
+            color: Theme.of(context).colorScheme.secondary,
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                  color: Theme.of(context).colorScheme.primary),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          buildListTile('Meals', Icons.restaurant),
+          buildListTile('Filters', Icons.settings)
+        ],
+      ),
+    );
+  }
+}
